4380cce34b8d69c01d5830fbaec21f1794bd49ca
Ellen <lemifi@mail.ru>
2022-01-13 00:12:46 +0300

    Waipoints set up

---
+++ Assets/EditModeRestrictions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    TextMesh textMesh;
+
+    void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize)* gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize)* gridSize;
+        transform.position = new Vector3(snapPos.x,0f,snapPos.z);
+
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
+}
